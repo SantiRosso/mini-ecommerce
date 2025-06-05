2c946fce07e4452acfbfdd503a9518e830049214
Santiago Rosso <santiagorosso@live.com.ar>
2025-06-05 17:26:07 -0300

    Update product detail component and styles

--- src/app/products/product-detail/product-detail.component.ts
+++ src/app/products/product-detail/product-detail.component.ts
@@ -1,12 +1,14 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { ProductService, Product } from '../product.service';
 import { CommonModule } from '@angular/common';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-product-detail',
   standalone: true,
   templateUrl: './product-detail.component.html',
+  styleUrls: ['./product-detail.component.scss'],
   imports: [CommonModule]
 })
 export class ProductDetailComponent implements OnInit {
@@ -14,11 +16,17 @@ export class ProductDetailComponent implements OnInit {
 
   constructor(
     private route: ActivatedRoute,
+    private router: Router,
+    private location: Location,
     private productService: ProductService
   ) {}
 
   ngOnInit(): void {
     const id = Number(this.route.snapshot.paramMap.get('id'));
     this.productService.getProductById(id).subscribe(p => this.product = p);
+  }
+
+  goBack(): void {
+    this.location.back();
   }
 }
